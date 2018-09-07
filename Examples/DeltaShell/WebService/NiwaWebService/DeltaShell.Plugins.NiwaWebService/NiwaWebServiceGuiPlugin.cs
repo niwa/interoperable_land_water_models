b38b6e5f7a3fb3cdc7f71c2db4f6b696a3b6f69a
Tom Evans <42591549+Tom-Evans-NIWA@users.noreply.github.com>
2018-09-07 14:33:41 +1200

    Uploaded NiwaWebServiceGuiPlugin.cs

---
+++ Examples/DeltaShell/WebService/NiwaWebService/DeltaShell.Plugins.NiwaWebService/NiwaWebServiceGuiPlugin.cs
@@ -0,0 +1,32 @@
+﻿using DelftTools.Shell.Core;
+using DelftTools.Shell.Gui;
+using Mono.Addins;
+
+namespace DeltaShell.Plugins.NiwaWebService
+{
+
+    [Extension(typeof(IPlugin))]
+    public class NiwaWebServiceGuiPlugin : GuiPlugin
+    {
+        public override string Name
+        {
+            get { return "NiwaWebServiceApplicationUI"; }
+        }
+        public override string DisplayName
+        {
+            get { return "NIWA Web Service application (UI)"; }
+        }
+        public override string Description
+        {
+            get { return "Gui plugin of the  Web Service Model application"; }
+        }
+        public override string Version
+        {
+            get { return "1.0"; }
+        }
+        public override string FileFormatVersion
+        {
+            get { return "1.0"; }
+        }
+    }
+}
