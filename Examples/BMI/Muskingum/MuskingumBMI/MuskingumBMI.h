2ad4fa8b4a3f9d7a7ef911ecabee1eaf71a24330
evansta <tom.evans@niwa.co.nz>
2018-11-02 15:17:48 +1300

    Made model conform to Open Earth BMI spec for use with Delta Shell and DIMR

--- Examples/BMI/Muskingum/MuskingumBMI/MuskingumBMI.h
+++ Examples/BMI/Muskingum/MuskingumBMI/MuskingumBMI.h
@@ -1,14 +1,11 @@
 #pragma once
 
-#ifdef MUSKINGUMBMI_EXPORTS
-#define MUSKINGUMBMI_API __declspec(dllexport)
-#else
-#define MUSKINGUMBMI_API __declspec(dllimport)
-#endif
-
 #include "MuskingumRouter.h"
+#include "bmi.h"
 #include <vector>
 #include <string>
+
+#define MAXVARNAMELEN 128
 
 extern "C" {
 
@@ -16,41 +13,42 @@ extern "C" {
 		friend class MuskingumYamlParser;
 
 	public:
-		MUSKINGUMBMI_API MuskingumBMI();
-		MUSKINGUMBMI_API ~MuskingumBMI();
+		BMI_API MuskingumBMI();
+		BMI_API ~MuskingumBMI();
 
 		// BMI Model Control Functions
-		MUSKINGUMBMI_API int Initialize(const char *config_file);
-		MUSKINGUMBMI_API int Update(void);
-		MUSKINGUMBMI_API void UpdateUntil(double then);
-		MUSKINGUMBMI_API void Finalize(void);
+		BMI_API int Initialize(const char *config_file);
+		BMI_API int Update(void);
+		BMI_API void UpdateUntil(double then);
+		BMI_API void Finalize(void);
 
 		// BMI Model Information Functions
-		MUSKINGUMBMI_API int GetInputVarNameCount(void) { return _reachVector.size(); }  // one input and one output [time series]
-		MUSKINGUMBMI_API int GetOutputVarNameCount(void) { return _reachVector.size(); } // for each reach
-		MUSKINGUMBMI_API void GetInputVarNames(char * const * const names);
-		MUSKINGUMBMI_API void GetOutputVarNames(char * const * const names);
+		BMI_API int GetInputVarNameCount(void) { return _reachVector.size(); }  // one input and one output [time series]
+		BMI_API int GetOutputVarNameCount(void) { return _reachVector.size(); } // for each reach
+		BMI_API void GetInputVarNames(char * const * const names);
+		BMI_API void GetOutputVarNames(char * const * const names);
 		const char* GetVarName(int i);
+		const int GetVarNameCount() { return _varCount; }
 
 		// BMI Time functions
-		MUSKINGUMBMI_API double GetTimeStep() { return timeStep; }
-		MUSKINGUMBMI_API std::string GetTimeUnits() { return "hours"; }
-		MUSKINGUMBMI_API double GetStartTime() { return startTime; }
-		MUSKINGUMBMI_API double GetCurrentTime() { return currentTime; }
-		MUSKINGUMBMI_API double GetEndTime() { return endTime; }
+		BMI_API double GetTimeStep() { return timeStep; }
+		BMI_API std::string GetTimeUnits() { return "hours"; }
+		BMI_API double GetStartTime() { return startTime; }
+		BMI_API double GetCurrentTime() { return currentTime; }
+		BMI_API double GetEndTime() { return endTime; }
 
 		// BMI Variable Information Functions
-		MUSKINGUMBMI_API int GetVarGrid(const char *  name);
-		MUSKINGUMBMI_API std::string GetVarUnits(const char * name) { return "m3 s-1"; } // all inputs and outputs are volume flows
-		MUSKINGUMBMI_API void GetVarType(const char * name, char * type);
-		MUSKINGUMBMI_API int GetVarItemsize(const char *name);
-		MUSKINGUMBMI_API int GetVarNbytes(const char *  name);
-		MUSKINGUMBMI_API std::string GetGridType(int id) { return "uniform_rectilinear"; };
-		MUSKINGUMBMI_API int GetGridRank(const char *  name) { return 0; } // all inputs and outputs are scalar
+		BMI_API int GetVarGrid(const char *  name);
+		BMI_API std::string GetVarUnits(const char * name) { return "m3 s-1"; } // all inputs and outputs are volume flows
+		BMI_API void GetVarType(const char * name, char * type);
+		BMI_API int GetVarItemsize(const char *name);
+		BMI_API int GetVarNbytes(const char *  name);
+		BMI_API std::string GetGridType(int id) { return "uniform_rectilinear"; };
+		BMI_API int GetGridRank(const char *  name) { return 0; } // all inputs and outputs are scalar
 
 		// BMI Variable Getter and Setter Functions
-		MUSKINGUMBMI_API void GetValue(std::string name, void* buffer);
-		MUSKINGUMBMI_API void SetValue(std::string name, void* buffer);
+		BMI_API void GetValue(std::string name, void** buffer);
+		BMI_API void SetValue(std::string name, const void* buffer);
 
 		double timeStep = NAN;
 		double startTime = 0.0;
@@ -61,24 +59,13 @@ extern "C" {
 
 	private:
 		std::vector<MuskingumRouter> _reachVector;
-		double* _dataArray;
+		// std::vector<double> _varValues;
+		double* _varValues;
+		char** _varNames;
+		int _varCount;
 		const char* getQuantityName(const char* name);
 	};
 
 	static MuskingumBMI* muskModel;
 
-	MUSKINGUMBMI_API int initialize(const char* config_file);
-	MUSKINGUMBMI_API int update(double);
-	MUSKINGUMBMI_API int finalize(void);
-	MUSKINGUMBMI_API void get_var_count(int*);
-	MUSKINGUMBMI_API void get_start_time(double* );
-	MUSKINGUMBMI_API void get_end_time(double* );
-	MUSKINGUMBMI_API void get_current_time(double*);
-	MUSKINGUMBMI_API void get_time_step(double*);
-	MUSKINGUMBMI_API void get_var(const char * key, void ** ref);
-	MUSKINGUMBMI_API void set_var(const char * key, const void * value);
-	MUSKINGUMBMI_API void get_var_rank(const char * key, int* value);
-	MUSKINGUMBMI_API void get_var_shape(const char * key, int** value);
-	MUSKINGUMBMI_API void get_var_type(const char * key, char* type);
-	MUSKINGUMBMI_API void get_var_name(int i, char*);
 }// end extern block
