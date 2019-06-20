b7811a03c18c1cea7f36315667e042a573d2b392
Tom Evans <42591549+Tom-Evans-NIWA@users.noreply.github.com>
2019-06-21 11:35:10 +1200

    Uploaded wrapper source

---
+++ SteadyState/SparrowRouter/wrapper/src/SparrowRouter.cpp
@@ -0,0 +1,112 @@
+// SparrowRouter.cpp : Defines the exported functions for the DLL application.
+//
+#include <cstdio>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include "bmi.h"
+#include <pybind11/embed.h>
+
+namespace py = pybind11;
+
+double current = 0;
+double timestep = 1;
+
+/* Store callback */
+Logger logger = NULL;
+
+/* Logger function */
+void _log(Level level, std::string msg);
+
+py::scoped_interpreter guard{}; // start the interpreter and keep it alive
+py::module srModule = py::module::import("SparrowRouter");
+// py::object srObj = srModule.attr("SparrowRouter")();
+
+int initialize(const char *config_file)
+{
+	std::ostringstream msg;
+	msg << "initializing with " << config_file;
+	_log(LEVEL_INFO, msg.str());
+
+	try
+	{
+		// py::print("In initialize");
+		srModule.attr("initialize")(config_file);
+	}
+	catch (py::error_already_set const &pythonErr)
+	{
+		std::cout << pythonErr.what();
+	}
+
+	return 0;
+}
+
+int update(double dt)
+{
+	std::ostringstream msg;
+	msg << "updating from " << current << " with dt: " << (dt != -1 ? dt : timestep);
+	_log(LEVEL_DEBUG, msg.str());
+	current += (dt != -1 ? dt : timestep);
+
+	try
+	{
+		// py::print("In update");
+		srModule.attr("update")(dt);
+	}
+	catch (py::error_already_set const &pythonErr)
+	{
+		std::cout << pythonErr.what();
+	}
+
+	return 0;
+}
+
+BMI_API int finalize()
+{
+	try {
+		// py::print("In finalize");
+		srModule.attr("finalize")();
+	}
+	catch (py::error_already_set const &pythonErr)
+	{
+		std::cout << pythonErr.what();
+	}
+	return 0;
+}
+
+
+void get_start_time(double *t)
+{
+	*t = 0;
+}
+
+void get_end_time(double *t)
+{
+	*t = 1;
+}
+
+void get_current_time(double *t)
+{
+	*t = current;
+}
+
+void get_time_step(double *dt)
+{
+	*dt = timestep;
+}
+
+void set_logger(Logger callback)
+{
+	Level level = LEVEL_INFO;
+	std::string msg = "Logging attached to cxx model";
+	logger = callback;
+	logger(level, msg.c_str());
+}
+
+void _log(Level level, std::string msg) {
+	if (logger != NULL) {
+		logger(level, msg.c_str());
+	}
+}
+
+
