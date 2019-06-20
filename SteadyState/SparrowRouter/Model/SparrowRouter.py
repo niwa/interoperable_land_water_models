1404c52e2f09b13deceb2e9e2be0c87a543d9e17
Tom Evans <42591549+Tom-Evans-NIWA@users.noreply.github.com>
2019-06-21 10:31:53 +1200

    Model restructured for BMI
    Python model split into initialize, update, and finalize functions. Name of database file can be set in configuration file read as argument to initialize function.

--- SteadyState/SparrowRouter/Model/SparrowRouter.py
+++ SteadyState/SparrowRouter/Model/SparrowRouter.py
@@ -1,10 +1,11 @@
 """ 
-Sparrow router. Takes input fluxes to stream segments in a drainage network, and accumulates the flux down the network with decay
-The inputs are read in from a sql database containing segment information, as described in a configuration file, and outputs are placed
+Takes input fluxes to stream segments in a drainage network, and accumulates the flux down the network with decay. \
+The inputs are read in from a sql database containing segment information, as described in a configuration file, and outputs are placed \
 into a further sql database. A BMI interface is provided for running the model, but input and output are file-based rather than through memory mapping.
+
 Sandy Elliott 2019
 """
-#region Imports and settings 
+#region Imports and global settings 
 import os
 import sys
 import numpy as np
@@ -16,8 +17,14 @@ import sqlite3
 import math
 from math import exp
 
+
+ProjectDir =''
+FileDb = "Sparrow.db" # can be overidden in configuration file
+modelHasInitialized = False
+modelHasRun = False
+
 pd.set_option('display.width', 150)
-#pylint:disable=E1101     
+# pylint:disable=E1101     
 # above disables the pylint no member error, as it was coming up inappropriately
 
 #endregion
@@ -25,105 +32,130 @@ pd.set_option('display.width', 150)
 #region           Initialise          
 #************************************************************
 
-# Read control and parameter files
-ProjectDir=''
+def initialize(config_file):
+    # Read control and parameter files
+    global FileDb, modelHasInitialized
+    global Segments, Parameters
 
-# Read parameters as yaml file
-with open(ProjectDir + 'Parameters.yaml', 'r') as f:
-    Parameters = yaml.load(f)
+    # Read parameters as yaml file
+    # with open(ProjectDir + 'Parameters.yaml', 'r') as f:
+    with open(os.path.join(ProjectDir, config_file), 'r') as f:
+        Parameters = yaml.load(f)
 
-# Obtain initial input variables for segments
+    try:
+        fn = Parameters["DatabaseFile"]
+        FileDb = fn
+    except KeyError:
+        pass
+
+    # Obtain initial input variables for segments from sqlite.
+    # (Note that the affinity types of the columns must be specified in the database,
+    # otherwise general 'object' dtypes are generated leading to later errors
+    # working with the variables, requiring specific type coercion later).
+    conn = sqlite3.connect(FileDb)
+    Segments = pd.read_sql_query("select * from Segments;", conn)
+    conn.close()
+    #From CSV
+    #Segments = pd.read_csv("Segments.csv")
 
 
-#From sqlite. (Note that the affinity types of the columns must be specified in the database, otherwise general 'object' dtypes are generated leading to later errors working with the
-# variables, requiring specific type coercion later).
-conn = sqlite3.connect("Sparrow.db")
-Segments = pd.read_sql_query("select * from Segments;", conn)
-conn.close()
-#From CSV
-#Segments = pd.read_csv("Segments.csv")
+    # Validate inputs (not implemented yet)
 
+    # sort by hydseq, first setting up the original sort order from row id
+    Segments['OriginalOrder'] = range(len(Segments))
+    Segments.sort_values('Hydseq',inplace=True)
 
-# Validate inputs (not implemented yet)
-
-# sort by hydseq, first setting up the original sort order from row id
-Segments['OriginalOrder'] = range(len(Segments))
-Segments.sort_values('Hydseq',inplace=True)
-
-
+    modelHasInitialized = True
 #endregion
 #************************************************************
 #region             Run calculations          
 #************************************************************
 
-nsegments = len(Segments)
+def update(dt):
+    global modelHasRun    
+    if modelHasRun or not modelHasInitialized:
+        return
 
-#convert dataframe columns to vectors for efficient calculations
-FromNodeVect = Segments.FromNode.values
-ToNodeVect = Segments.ToNode.values
-
-# Generate dictionary of Nodes. key is node identifier, value is load
-NodeLoad = {}
-# build up dictionary based on the from and to nodes, with intial value of 0
-for isegment in range(nsegments):
-    fromnode = FromNodeVect[isegment]
-    if not(fromnode in NodeLoad):
-        NodeLoad[fromnode] = 0. 
-    tonode = ToNodeVect[isegment]
-    if not(tonode in NodeLoad):
-        NodeLoad[tonode] = 0. 
-
-Segments['SourceTotal'] = Segments.SourceDirect + Segments.SourceGroundwater + Segments.SourcePoint
-
-# Calculate decay fraction
-def StreamDecay(a,b,Length,Flow):
-    Flow = np.maximum(Flow,0.001)
-    DecayCoefficient = a * Flow**b
-    StreamCarry = np.exp(-DecayCoefficient*Length)
-    return StreamCarry
-#** pretty ugly using the np math functions above to enable vectorised calculations. But the function does work OK with scalars too, so no harm. Could use Numba to create own universal function ufunc with a scalar-based function
+    # Calculate decay fraction
+    def StreamDecay(a,b,Length,Flow):
+        Flow = np.maximum(Flow,0.001)
+        DecayCoefficient = a * Flow**b
+        StreamCarry = np.exp(-DecayCoefficient*Length)
+        return StreamCarry
+    #** pretty ugly using the np math functions above to enable vectorised calculations. 
+    # But the function does work OK with scalars too, so no harm. 
+    # Could use Numba to create own universal function ufunc with a scalar-based function
 
 
-Segments['StreamCarry'] = StreamDecay(Parameters['StreamDecayCoefficient'],Parameters['StreamDecayExponent'],Segments.Length.values,Segments.Flow.values)
+    nsegments = len(Segments)
 
-# Route the loads
+    #convert dataframe columns to vectors for efficient calculations
+    FromNodeVect = Segments.FromNode.values
+    ToNodeVect = Segments.ToNode.values
 
-SourceTotalVect = Segments.SourceTotal.values  
-StreamCarryVect = Segments.StreamCarry.values
-LoadVec = np.zeros(nsegments)
+    # Generate dictionary of Nodes. key is node identifier, value is load
+    NodeLoad = {}
+    # build up dictionary based on the from and to nodes, with intial value of 0
+    for isegment in range(nsegments):
+        fromnode = FromNodeVect[isegment]
+        if not(fromnode in NodeLoad):
+            NodeLoad[fromnode] = 0. 
+        tonode = ToNodeVect[isegment]
+        if not(tonode in NodeLoad):
+            NodeLoad[tonode] = 0. 
 
-for segment in range(nsegments):
-    streamcarry = StreamCarryVect[segment]
-    load = NodeLoad[FromNodeVect[segment]] * streamcarry + SourceTotalVect[segment] * streamcarry**0.5
-    tonode = ToNodeVect[segment]
-    NodeLoad[tonode] += load # Add the segment outlet load to the node load.
-    LoadVec[segment] = load
+    Segments['SourceTotal'] = Segments.SourceDirect + Segments.SourceGroundwater + Segments.SourcePoint
 
-Segments['SegmentLoad'] = LoadVec
+    Segments['StreamCarry'] = StreamDecay(Parameters['StreamDecayCoefficient'],Parameters['StreamDecayExponent'],Segments.Length.values,Segments.Flow.values)
+
+    # Route the loads
+
+    SourceTotalVect = Segments.SourceTotal.values  
+    StreamCarryVect = Segments.StreamCarry.values
+    LoadVec = np.zeros(nsegments)
+
+    for segment in range(nsegments):
+        streamcarry = StreamCarryVect[segment]
+        load = NodeLoad[FromNodeVect[segment]] * streamcarry + SourceTotalVect[segment] * streamcarry**0.5
+        tonode = ToNodeVect[segment]
+        NodeLoad[tonode] += load # Add the segment outlet load to the node load.
+        LoadVec[segment] = load
+
+    Segments['SegmentLoad'] = LoadVec
+    modelHasRun = True
 #endregion
 #************************************************************
 #region                 Finalise          
 #************************************************************
 
-# Output the variables
+def finalize():
+    global FileDb, modelHasRun
 
-Segments.sort_values('OriginalOrder',inplace=True)
+    if not modelHasRun:
+        return
+    # Output the variables
 
-# check that the sqlite can be opened in write mode (not implemented yet)
+    Segments.sort_values('OriginalOrder',inplace=True)
+
+    # check that the sqlite can be opened in write mode (not implemented yet)
 
 
-# write the table
+    # write the table
 
-conn = sqlite3.connect('Sparrow.db')
+    conn = sqlite3.connect(FileDb)
 
-SegmentLoad = Segments[['SegmentID','SegmentLoad']]
-conn.execute('DROP TABLE if exists SegmentLoad')
-SegmentLoad.to_sql('SegmentLoad', conn) 
+    SegmentLoad = Segments[['SegmentID','SegmentLoad']]
+    conn.execute('DROP TABLE if exists SegmentLoad')
+    SegmentLoad.to_sql('SegmentLoad', conn) 
 
-conn.execute('DROP TABLE if exists SegmentsNew')
-conn.execute('Create table SegmentsNew as Select Segments.*, SegmentLoad.SegmentLoad from Segments LEFT JOIN SegmentLoad Using(SegmentID)')
+    conn.execute('DROP TABLE if exists SegmentsNew')
+    conn.execute('Create table SegmentsNew as Select Segments.*, SegmentLoad.SegmentLoad from Segments LEFT JOIN SegmentLoad Using(SegmentID)')
 
-conn.close()
+    conn.close()
 
 #endregion
 
+if __name__ == "__main__":
+    initialize("Parameters.yaml")
+    update(0)
+    finalize()
